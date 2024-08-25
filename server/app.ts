d55b803cc0d16bf9e3f4e5a9fd8cd9d868a2b4dc
agenovia <aarongenovia@gmail.com>
2024-08-25 14:28:05 -0700

    removed firebase, added supabase skeletion

--- server/app.ts
+++ server/app.ts
@@ -1,27 +1,36 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
+import * as dotenv from "dotenv";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import Vault from "./lib/vault.js";
 import { default as v1 } from "./routes/v1/router.js";
-import Vault from "./src/vault.js";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(morgan("combined"));
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
-// fetch pinecone key from hashicorp vault
+// access hcp vault
 const vault = new Vault(
   process.env.HCP_CLIENT_ID ?? "",
   process.env.HCP_CLIENT_SECRET ?? "",
   process.env.HCP_VAULT_URL ?? ""
 );
 
-// fetch our database key
-const pineconeSk = (await vault.fetchSecret("pinecone"))?.value;
+// fetch our secrets
+const [pineconeSk, supabase_url, supabase_anon_key] = [
+  (await vault.fetchSecret("pinecone"))?.value,
+  (await vault.fetchSecret("supabase_url"))?.value,
+  (await vault.fetchSecret("supabase_anon_key"))?.value,
+];
 
-app.use("/v1", v1);
+app.use("/", v1);
 
 app.listen(42000, () => {
   console.log("App listening on port 42000");
